f2e94bff0e6c9529a44be91ed6ae779770666cde
gaotianye <15156506755@163.com>
2017-02-18 23:13:07 +0800

    update：修改了client端测试代码

--- designPatterns/src/main/java/cn/celloud/designPatterns/simpleFactory/example1/Client.java
+++ designPatterns/src/main/java/cn/celloud/designPatterns/simpleFactory/example1/Client.java
@@ -2,7 +2,7 @@ package cn.celloud.designPatterns.simpleFactory.example1;
 
 public class Client {
 	public static void main(String[] args) throws Exception {
-		Caculate instance = CaculateFactory.getInstance("1");
+		Caculate instance = CaculateFactory.getInstance("*");
 		double result = instance.caculte(4, 0);
 		System.out.println("结果是："+result);
 	}
