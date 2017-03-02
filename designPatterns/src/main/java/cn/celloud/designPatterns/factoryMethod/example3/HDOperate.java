bfb3155764b0a7d848b7f58b4334e63adc66e650
gaotianye <15156506755@163.com>
2017-03-02 14:55:05 +0800

    update：补充注解

--- designPatterns/src/main/java/cn/celloud/designPatterns/factoryMethod/example3/HDOperate.java
+++ designPatterns/src/main/java/cn/celloud/designPatterns/factoryMethod/example3/HDOperate.java
@@ -1,5 +1,9 @@
 package cn.celloud.designPatterns.factoryMethod.example3;
-
+/**
+ * 硬盘功能
+ * @author Administrator
+ *
+ */
 public interface HDOperate {
 	//获取数据
 	String getData();
--- designPatterns/src/main/java/cn/celloud/designPatterns/factoryMethod/example3/HardDisk.java
+++ designPatterns/src/main/java/cn/celloud/designPatterns/factoryMethod/example3/HardDisk.java
@@ -1,5 +1,9 @@
 package cn.celloud.designPatterns.factoryMethod.example3;
-
+/**
+ * HardDisk:硬盘
+ * @author Administrator
+ *
+ */
 public abstract class HardDisk {
 	//创建HD操作对象
 	protected abstract HDOperate createHDOperate();
